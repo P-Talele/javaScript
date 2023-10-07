4131561a18101a400df223c5d288c9ca4cf8c67c
prashant <prashanttalele1997@gmail.com>
2023-10-07 18:54:50 +0530

    commit

--- 16_interation.js
+++ 16_interation.js
@@ -1,7 +1,7 @@
 for (let index = 0; index <=10; index++) 
      {
-     const element = index
-     console.log(element)
+       const element = index
+       console.log(element)
     
     }
      console.log("*******************Array***************************")
@@ -17,7 +17,7 @@ for (let index = 0; index <=10; index++)
     for (let index = 0; index < myArray.length; index++) 
       {
             const element = myArray[index];
-            console.log(element)
+              console.log(element)
       }
     console.log("*******************Array***************************")
     const myarray=["prashant","Arun","Talele"]
@@ -35,7 +35,34 @@ for (let index = 0; index <=10; index++)
     {
          console.log(ele);
     }
- 
+    console.log("*******************function***************************") 
+
+        const  programmming=["c","c++","java","Python","ruby"]
+        programmming.forEach(function(item)
+      {
+                console.log(item);
+      });
+
+      console.log("******************* forEach loop Using Arrow Fucton ***************************") 
+
+      const  language=["c","c++","java","Python","ruby"]
+      language.forEach((item ,index ,language) =>
+        {
+           console.log(item ,index, language);
+        })
+       console.log("******************* forEach loop Using Arrow Fucton ***************************") 
+       
+         const student =[{
+                  Ashish:"Java Developer",
+                  Vijay:"India Mart",
+                  Prashant:"Java Developer"
+                  }]     
+                student.forEach((item)=>{
+                     console.log(item)
+           });
+
+   
+
 
  
    
