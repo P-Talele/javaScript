5eb904e93a2fd6135f581a9515e5226f01fd98f1
prashant <prashanttalele1997@gmail.com>
2023-10-08 14:27:22 +0530

    Project 1 Completed

---
+++ javaScript_Project/jscript.js
@@ -0,0 +1,27 @@
+const button= document.querySelectorAll(`.box`)
+const body = document.querySelector(`.body`)
+
+
+    button.forEach(function(box)
+     {
+          box.addEventListener("click",function(e) {
+            if(e.target.id ==="red")
+            {   
+                body.style.backgroundColor=e.target.id;
+            }
+            if(e.target.id ==="white")
+            {   
+                body.style.backgroundColor=e.target.id;
+            }
+            if(e.target.id ==="yellow")
+            {   
+                body.style.backgroundColor=e.target.id;
+            }
+            if(e.target.id ==="gray")
+            {   
+                body.style.backgroundColor=e.target.id;
+            }
+          })
+    })
+    
+
