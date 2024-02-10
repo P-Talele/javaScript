4dd09d268a4f01df9de5b922fbab388bf0a00d3a
prashant <prashanttalele1997@gmail.com>
2024-02-10 12:15:49 +0530

    commit

--- 01_Javascript_Practice/03_Programs/04_Pelindrome_Number.js
+++ 01_Javascript_Practice/03_Programs/04_Pelindrome_Number.js
@@ -1,1 +1,20 @@
-  
+let num1 =121
+let rem;
+let rev=0
+let temp=num1
+
+
+ while(num1>0)
+ {
+        rem=num1%10
+        rev=rev*10+rem
+        num1=num1/10
+ }
+console.log(rev)
+ if(temp==rev)
+ {
+     console.log("This is Pelindrome")
+ }
+ else{
+    console.log("This is  not Pelimdrome Number")
+ }
