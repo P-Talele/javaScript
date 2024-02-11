cad16c4cdadf9405d49a4deb27db4cdffea13c41
prashant <prashanttalele1997@gmail.com>
2024-02-11 12:19:42 +0530

    commit

---
+++ 03_Constructor.js
@@ -0,0 +1,13 @@
+class Student 
+{
+     constructor(id , name)
+     {
+          this.id=id
+          this.name=name
+     }
+ }
+      let obj=new Student(121,"Dimpal")
+      console.log(obj)
+      console.log(obj.id)
+      console.log(obj.name)
+  
---
+++ 04_Static_Method.js
@@ -0,0 +1,32 @@
+class Student 
+{
+      static disp()
+       {
+            let name =" my name is prashant"
+             return name
+       }
+}
+ 
+    console.log(Student.disp())
+
+
+
+class Student1
+{
+       static disp1()
+       {
+           let name =" my name is prashant"
+            return name
+      }
+
+       static disp2()
+      {
+              let name =" my name is  Rohini"
+              return name
+      }
+
+
+}
+
+console.log(Student1.disp1())
+console.log(Student1.disp2())
